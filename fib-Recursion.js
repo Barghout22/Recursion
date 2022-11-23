aee053fc3f94cfbeada7652182417afd07b13839
Mahmoud <barghoutmahmoud2@gmail.com>
2022-11-23 18:48:12 +0200

    added 2 fibonacci functions. one calculates singular fib values and the other calculates an array of fib values.

--- fib-Recursion.js
+++ fib-Recursion.js
@@ -1,9 +1,19 @@
-function fibRe(num, sequence = [0, 1]) {
-  if (num < 2) {
-    return sequence.slice(0, num);
-  }
-
-  return sequence;
+function fibRe(num) {
+  if (num == 0) return [0];
+  else if (num == 1) return [0, 1];
+  const arr = fibRe(num - 1);
+  return [...arr, arr[num - 1] + arr[num - 2]];
 }
 
-console.log(fibRe(1));
+console.log(fibRe(5));
+
+function fibReSingularValue(num) {
+  if (num == 1) {
+    return 0;
+  } else if (num == 2) {
+    return 1;
+  }
+  return fibReSingularValue(num - 1) + fibReSingularValue(num - 2);
+}
+
+//console.log(fibReSingularValue(5));
